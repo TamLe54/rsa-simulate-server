f53c5535c9cc35a910794c66aa902dd76fcf1caf
Lê Trí Tâm <tam.le@techsoft.asia>
2023-11-11 21:16:56 +0700

    avoid magic string for string type

--- src/app/controllers/rsa.controller.js
+++ src/app/controllers/rsa.controller.js
@@ -3,7 +3,7 @@ const crypto = require('crypto')
 const hasString = require('../util/hashString')
 const encryption = require('../util/encrypt')
 const decryption = require('../util/decrypt')
-const { MODULE_LENGTH } = require('../constants')
+const { MODULE_LENGTH, STRING_TYPE } = require('../constants')
 const bufferHandle = require('../util/bufferHandle')
 
 class RSAController {
@@ -11,10 +11,8 @@ class RSAController {
     console.log('Starting key generating....')
     const { email, password } = req.body
 
-    //* Create randomly salt
     const salt = crypto.randomBytes(32)
 
-    //* Use PBKDF2 to create a secret key from password
     crypto.pbkdf2(password, salt, 10000, 32, 'sha512', async (err, key) => {
       if (err) throw err
 
@@ -23,7 +21,7 @@ class RSAController {
           {
             email: email,
             hashPassword: hasString(password),
-            passphrase: key.toString('hex'),
+            passphrase: key.toString(STRING_TYPE.HEX),
           },
           {
             onConflict: ['email'],
@@ -33,7 +31,7 @@ class RSAController {
         crypto.generateKeyPair(
           'rsa',
           {
-            modulusLength: MODULE_LENGTH, // the key length
+            modulusLength: MODULE_LENGTH,
             publicKeyEncoding: {
               type: 'spki',
               format: 'pem',
@@ -41,8 +39,8 @@ class RSAController {
             privateKeyEncoding: {
               type: 'pkcs8',
               format: 'pem',
-              cipher: 'aes-256-cbc', //Cipher the private key
-              passphrase: key.toString('hex'), // use the secret key above to hide the private key
+              cipher: 'aes-256-cbc',
+              passphrase: key.toString(STRING_TYPE.HEX),
             },
           },
           (err, publicKey, privateKey) => {
@@ -60,8 +58,6 @@ class RSAController {
       } catch (err) {
         throw err
       }
-
-      // Create RSA key pars
     })
   }
 
@@ -71,22 +67,18 @@ class RSAController {
     console.log('Starting Encryption...')
 
     try {
-      //* split the data into smaller buffers
       const chunks = bufferHandle.splitUTF8(dataToEncrypt)
 
-      //* encrypt each chunk of buffer
       const encryptedChunks = chunks.map((chunk) =>
         encryption(chunk, publicKey)
       )
 
-      //* join the encrypted buffers into one buffer and convert to base64 string
       const encryptedData = bufferHandle
         .join(encryptedChunks)
-        .toString('base64')
+        .toString(STRING_TYPE.BASE64)
 
       console.log('Completed Encryption')
 
-      //* return the base64 string
       return res.status(200).json({
         encryptedData: encryptedData,
       })
@@ -100,18 +92,15 @@ class RSAController {
 
     console.log('Starting Decryption....')
 
-    //* hash the gotten password
     const hashPassword = hasString(password)
 
     try {
-      //*compare the hash with the hash in database corresponding with email
       const { data, error } = await supabase
         .from('RSA_Account')
         .select('passphrase')
         .eq('email', email)
         .eq('hashPassword', hashPassword)
 
-      //* return the error if checking process is down
       if (error) {
         return res.status(500).json({
           success: 'false',
@@ -119,7 +108,6 @@ class RSAController {
         })
       }
 
-      //* return the error when no data in database match the requested data
       if (data.length === 0) {
         return res.status(404).json({
           success: 'false',
@@ -127,23 +115,19 @@ class RSAController {
         })
       }
 
-      //* get the passphrase the was stored in the database
       const passphrase = data[0].passphrase
-
-      //* split the encrypted data into smaller buffer called chunks
       const chunks = bufferHandle.splitBase64(encryptedData)
 
-      //* decrypt each of chunk
       const decryptedChunks = chunks.map((chunk) =>
         decryption(chunk, privateKey, passphrase)
       )
 
-      //* join all chunks into one buffer then convert it into utf-8 string
-      const decryptedData = bufferHandle.join(decryptedChunks).toString('utf-8')
+      const decryptedData = bufferHandle
+        .join(decryptedChunks)
+        .toString(STRING_TYPE.UTF8)
 
       console.log('Completed Decryption!')
 
-      //* return the result
       return res.status(200).json({
         success: 'true',
         decryptedData: decryptedData,
--- src/app/util/hashString.js
+++ src/app/util/hashString.js
@@ -1,13 +1,10 @@
 const crypto = require('crypto')
 
 const hasString = (inputString) => {
-  // Create a new SHA-512 hash object
   const sha512 = crypto.createHash('sha512')
 
-  // Update the hash object with the input string
   sha512.update(inputString, 'utf-8')
 
-  // Get the hexadecimal representation of the hash
   const hashedString = sha512.digest('hex')
 
   return hashedString
